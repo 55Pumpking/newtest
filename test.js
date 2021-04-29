937a556c43e76536a1bc095b4128fe39f5a1a91e
Lin <1017333709@qq.com>
2021-04-29 14:50:27 +0800

    2.0

--- test.js
+++ test.js
@@ -1,29 +1,16 @@
-function bubbleSort(arr) {
+function shellSort(arr) {
 
-    var len = arr.length;
+    var len = arr.length,
     
-    for (var i = 0; i < len; i++) {
+    temp,
     
-    for (var j = 0; j < len - 1 - i; j++) {
+    gap = 1;
     
-       if (arr[j] > arr[j+1]) {//相邻元素两两对比
+    console.time('希尔排序耗时:');
     
-       var temp = arr[j+1];//元素交换
+    while(gap < len/5) {//动态定义间隔序列
     
-             arr[j+1] = arr[j];
+    gap =gap*5+1;
     
-           arr[j] = temp;
-    
-    }
-    
-    }
-    
-    }
-    
-    return arr;
-    
-    }
-    
-    var arr=[3,44,38,5,47,15,36,26,27,2,46,4,19,50,48];
-    
-    console.log(bubbleSort(arr));//[2, 3, 4, 5, 15, 19, 26, 27, 36, 38, 44, 46, 47, 48, 50]
+}
+}
